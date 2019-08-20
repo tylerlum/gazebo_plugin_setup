42f9f92f77a81490e6fcb0367d1f07b94cb36743
Ian Chen <ichen@osrfoundation.org>
2019-08-19 18:16:10 -0700

    update standalone example, add fresnel plane model

--- ocean_visual.cc
+++ ocean_visual.cc
@@ -61,11 +61,15 @@ namespace asv
 
     // OGRE
     public: Ogre::RenderTarget *renderTarget;
+    public: Ogre::RenderTarget *refractionRt;
     public: Ogre::Camera *camera;
+    public: Ogre::Camera *trackedCamera;
     public: Ogre::SceneNode *planeNode;
     public: gazebo::rendering::ScenePtr scene;
-    public: Ogre::MovablePlane* plane;
+    public: Ogre::Plane plane;
     public: Ogre::Entity* planeEntity;
+    public: Ogre::TexturePtr rttReflectionTexture;
+    public: Ogre::TexturePtr rttRefractionTexture;
   };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -89,12 +93,6 @@ namespace asv
     sdf::ElementPtr _sdf)
   {
     gzerr << "IN OCEAN VISUAL" << std::endl;
-    // Only load plugin once, even though the model.xacro asks for 2
-    static int i = 0;
-    i++;
-    if (i == 2) { return; }
-    Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-
 
     // Capture visual and plugin SDF
     GZ_ASSERT(_visual != nullptr, "Visual must not be null");
@@ -107,65 +105,122 @@ namespace asv
     // OGRE setup
     this->data->scene = _visual->GetScene();
 
+    // Only load plugin once
+    // load the visual plugin in gzclient for now.
+    if (!this->data->scene->EnableVisualizations())
+      return;
+
+    // Setup camera
+    Ogre::Camera *userCamera = this->data->scene->GetUserCamera(0)->OgreCamera();
+    if (userCamera)
+    {
+      this->data->trackedCamera = userCamera;
+      this->data->camera =
+        this->data->scene->OgreSceneManager()->createCamera(
+        this->data->visual->Name() + "_fresnelCam");
+
+      Ogre::SceneNode *node =
+          this->data->scene->OgreSceneManager()->getRootSceneNode()->
+          createChildSceneNode();
+      node->attachObject(this->data->camera);
+    }
+    else
+    {
+      gzerr << "User camera not found" << std::endl;
+      return;
+    }
+
+
+    Ogre::SceneNode *ogreNode = _visual->GetSceneNode();
+    this->data->planeEntity =
+        dynamic_cast<Ogre::Entity *>(ogreNode->getAttachedObject(0));
+    if (!this->data->planeEntity)
+    {
+      gzerr << "No plane entity found" << std::endl;
+      return;
+    }
+
     // Create Plane for reflection texture
-    this->data->plane = new Ogre::MovablePlane("Plane");
-    this->data->plane->d = 0;
-    this->data->plane->normal = Ogre::Vector3::UNIT_Z;
-    Ogre::MeshManager::getSingleton().createPlane(
-      "PlaneMesh",
-      Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-      *(this->data->plane),
-      512, 512, 1, 1,
-      true,
-      1, 1, 1,
-      Ogre::Vector3::UNIT_Y);
-    (this->data->planeEntity = this->data->scene->OgreSceneManager()->
-     createEntity("PlaneMesh"));
-    (this->data->planeNode = this->data->scene->OgreSceneManager()->
-     getRootSceneNode()->createChildSceneNode());
-    this->data->planeNode->attachObject(this->data->planeEntity);
-    this->data->planeNode->attachObject(this->data->plane);
+    // this->data->plane = new Ogre::MovablePlane("Plane");
+    // this->data->plane->d = 0;
+    // this->data->plane->normal = Ogre::Vector3::UNIT_Z;
+    this->data->plane = Ogre::Plane(Ogre::Vector3::UNIT_Z, 0);
+    // Ogre::MeshManager::getSingleton().createPlane(
+    //   "PlaneMesh2",
+    //   Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+    //   this->data->plane,
+    //   512, 512, 1, 1,
+    //   true,
+    //   1, 1, 1,
+    //   Ogre::Vector3::UNIT_Y);
+    // this->data->planeEntity = this->data->scene->OgreSceneManager()->
+    //     createEntity("PlaneMesh", "PlaneMesh2");
+    // (this->data->planeNode = this->data->scene->OgreSceneManager()->
+    //  getRootSceneNode()->createChildSceneNode());
+    // this->data->planeNode->attachObject(this->data->planeEntity);
+    // this->data->planeNode->attachObject(this->data->plane);
 
     // QUESTION: Create render texture, if I give it the same name as
     // the texture in scripts/waves.material, it would not work for some reason
-    Ogre::TexturePtr rttTexture =
+    this->data->rttReflectionTexture =
       Ogre::TextureManager::getSingleton().createManual(
-        "mytexture",
+        this->data->visual->Name() + "_reflection",
         Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
         Ogre::TEX_TYPE_2D,
         512, 512,
         0,
         Ogre::PF_R8G8B8,
         Ogre::TU_RENDERTARGET);
-    this->data->renderTarget = rttTexture->getBuffer()->getRenderTarget();
 
-    // Setup camera
-    this->data->camera = this->data->scene->GetUserCamera(0)->OgreCamera();
+    this->data->rttRefractionTexture =
+      Ogre::TextureManager::getSingleton().createManual(
+        this->data->visual->Name() + "_refraction",
+        Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+        Ogre::TEX_TYPE_2D,
+        512, 512,
+        0,
+        Ogre::PF_R8G8B8,
+        Ogre::TU_RENDERTARGET);
+
+    this->data->renderTarget =
+        this->data->rttReflectionTexture->getBuffer()->getRenderTarget();
+    this->data->renderTarget->setAutoUpdated(false);
 
     // Setup render texture
-    this->data->renderTarget->addViewport(this->data->camera);
-    this->data->renderTarget->getViewport(0)->setClearEveryFrame(true);
-    (this->data->renderTarget->getViewport(0)->
-     setBackgroundColour(Ogre::ColourValue::Black));
-
-    Ogre::MaterialPtr renderMaterial =
-      Ogre::MaterialManager::getSingleton().create(
-        "mymat",
-        Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-    Ogre::TextureUnitState* t = (renderMaterial->getTechnique(0)->getPass(0)->
-      createTextureUnitState("mytexture"));
-
-    t->setTextureAddressingMode(Ogre::TextureUnitState::TAM_CLAMP);
-    t->setProjectiveTexturing(true, this->data->camera);
+    Ogre::Viewport *vp = this->data->renderTarget->addViewport(this->data->camera);
+    vp->setClearEveryFrame(true);
+    vp->setOverlaysEnabled(false);
+    vp->setVisibilityMask(GZ_VISIBILITY_ALL &
+        ~(GZ_VISIBILITY_GUI | GZ_VISIBILITY_SELECTABLE));
+    rendering::RTShaderSystem::AttachViewport(vp, this->data->scene);
     this->data->renderTarget->addListener(this);
+
+    this->data->refractionRt =
+        this->data->rttRefractionTexture->getBuffer()->getRenderTarget();
+    this->data->refractionRt->setAutoUpdated(false);
+    vp = this->data->refractionRt->addViewport(this->data->camera);
+    vp->setClearEveryFrame(true);
+    vp->setOverlaysEnabled(false);
+    vp->setAutoUpdated(false);
+    vp->setVisibilityMask(GZ_VISIBILITY_ALL &
+        ~(GZ_VISIBILITY_GUI | GZ_VISIBILITY_SELECTABLE));
+    rendering::RTShaderSystem::AttachViewport(vp, this->data->scene);
+    this->data->refractionRt->addListener(this);
+
+    Ogre::MaterialPtr origMat =
+        Ogre::MaterialManager::getSingleton().getByName("fresnel");
+    Ogre::MaterialPtr mat = origMat->clone(this->data->visual->Name() + "_mat");
+    Ogre::TextureUnitState *reflectTex =
+        mat->getTechnique(0)->getPass(0)->getTextureUnitState(1);
+    reflectTex->setTexture(this->data->rttReflectionTexture);
+    Ogre::TextureUnitState *refractTex =
+        mat->getTechnique(0)->getPass(0)->getTextureUnitState(2);
+    refractTex->setTexture(this->data->rttRefractionTexture);
 
     // Camera reflection and clip plane setup
     this->data->camera->enableReflection(this->data->plane);
-    this->data->camera->enableCustomNearClipPlane(this->data->plane);
-    this->data->planeEntity->setMaterialName("mymat");
-
-    // Show rendertexture on oceanwaves, not well scaled or positioned
-    // this->data->visual->SetMaterial("mymat");
+    // this->data->camera->enableCustomNearClipPlane(this->data->plane);
+    this->data->planeEntity->setMaterialName(mat->getName());
 
     // Bind the update method to ConnectPreRender events
     this->data->connection = event::Events::ConnectRender(
@@ -174,37 +229,73 @@ namespace asv
 
   void WavefieldVisualPlugin::OnUpdate()
   {
-    if (this->data->renderTarget)
-    {
-      this->data->renderTarget->update();
-    }
+    if (!this->data->camera || !this->data->trackedCamera ||
+        !this->data->renderTarget)
+      return;
+
+    this->data->camera->setCustomProjectionMatrix(true,
+      this->data->trackedCamera->getProjectionMatrix());
+    this->data->camera->setPosition(
+        this->data->trackedCamera->getDerivedPosition());
+    this->data->camera->setOrientation(
+        this->data->trackedCamera->getDerivedOrientation());
+
+    this->data->renderTarget->update();
+    this->data->renderTarget->writeContentsToFile("asdf.png");
+
+//    this->data->camera->getViewport()->getTarget()->update();
+//    this->data->camera->getViewport()->getTarget()->writeContentsToFile("c.png");
+
+
+//    this->data->trackedCamera->getViewport()->getTarget()->update();
+//    this->data->trackedCamera->getViewport()->getTarget()->writeContentsToFile("start.png");
   }
 
   void WavefieldVisualPlugin::preRenderTargetUpdate(
       const Ogre::RenderTargetEvent& rte)
   {
+    if (!this->data->camera)
+      return;
+
     if (this->data->planeEntity)
     {
       this->data->planeEntity->setVisible(false);
     }
-    if (this->data->camera)
+
+    // reflection
+    if (rte.source == this->data->renderTarget)
     {
       this->data->camera->enableReflection(this->data->plane);
-      this->data->camera->enableCustomNearClipPlane(this->data->plane);
+      // this->data->camera->enableCustomNearClipPlane(this->data->plane);
+    }
+    // refraction
+    else
+    {
+      // TODO
     }
   }
 
   void WavefieldVisualPlugin::postRenderTargetUpdate(
       const Ogre::RenderTargetEvent& rte)
   {
+    if (!this->data->camera)
+      return;
+
     if (this->data->planeEntity)
     {
       this->data->planeEntity->setVisible(true);
     }
-    if (this->data->camera)
+
+    // reflection
+    if (rte.source == this->data->renderTarget)
     {
       this->data->camera->disableReflection();
-      this->data->camera->disableCustomNearClipPlane();
+      // this->data->camera->disableCustomNearClipPlane();
+    }
+    // refraction
+    else
+    {
+      // TODO
     }
   }
 }
